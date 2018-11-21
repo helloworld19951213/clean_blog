7dfcb23782f081cffa621d8bee481d02fbd7f008
jinhao <im7h1995@hotmail.com>
2018-11-21 09:22:12 +0800

    fix

--- cleanblog/settings.py
+++ cleanblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_m9c!59*^eq8dpi9ntu$c0m32o4sgkaiu2ed$r@3^cftk@cbhs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
